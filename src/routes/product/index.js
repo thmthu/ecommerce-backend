877e35b5ff290fdca967ed9fb487d3809467053e
thmthu <thmt.thu@gmail.com>
2025-02-02 19:11:36 +0700

    add middleware check vadilation

--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -2,7 +2,9 @@
 const ProductController = require("../../controllers/product.controller");
 const express = require("express");
 const { asyncHandler } = require("../../auth/checkAuth");
+const { authentication } = require("../../auth/authUtils");
 const router = express.Router();
+router.use(authentication);
 router.post("/", asyncHandler(ProductController.createProduct));
 router.get("/get-all", asyncHandler(ProductController.getAllProducts));
 router.get("/:id", asyncHandler(ProductController.getProductById));
