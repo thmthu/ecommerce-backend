284ddf105478cd202694fc80627ccb284b20863b
thmthu <thmt.thu@gmail.com>
2025-02-12 14:58:31 +0700

    add utils getSelect and unselect for mongo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,22 @@
 "use strict";
 const _ = require("lodash");
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Types;
 const getInforData = ({ fields = [], object = {} }) => {
   return _.pick(object, fields);
 };
-
-module.exports = { getInforData };
+const getSelectData = (select = []) => {
+  return Object.fromEntries(select.map((field) => [field, 1]));
+};
+const unGetSelectData = (unselect = []) => {
+  return Object.fromEntries(unselect.map((field) => [field, 0]));
+};
+const convertStringToObjectId = (str) => {
+  return new ObjectId(str);
+};
+module.exports = {
+  getInforData,
+  convertStringToObjectId,
+  getSelectData,
+  unGetSelectData,
+};
